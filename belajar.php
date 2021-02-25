ae30977391401f333582ae01c8e9244aea6a2d79
fif122 <m.afif2277@gmail.com>
2021-02-25 15:26:14 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nama = "Afif";
+    $angka = 12;
+
+    echo "Nama saya adalah " .$nama;
+
+    echo "<br>";
+    echo $angka;
+
+    echo "<br>";
+    echo "hello world!";
+    echo "<h1>hello world</h1>";
+
+?>
